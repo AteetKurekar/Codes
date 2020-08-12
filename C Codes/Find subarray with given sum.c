b2f0e49279255a1c232fcc29f96a4c2a1ea45af0
Ateet Kurekar <60309207+AteetKurekar@users.noreply.github.com>
2020-08-12 10:49:36 +0530

    Create Find subarray with given sum.c

---
+++ C Codes/Find subarray with given sum.c
@@ -0,0 +1,37 @@
+Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+Ouptut: Sum found between indexes 2 and 4
+Sum of elements between indices
+2 and 4 is 20 + 3 + 10 = 33
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+	
+	int t;scanf("%d",&t);
+	while(t>0){
+		int n;scanf("%d",&n);
+		int num;scanf("%d",&num);
+		int arr[n];
+		for(int i=0;i<n;i++){
+			scanf("%d",&arr[i]);
+		}
+	//	int arr[]={1, 4, 0, 0, 3, 10, 5};
+	//	int n=sizeof(arr)/sizeof(arr[0]);
+		int i,j;
+		for(i=0;i<n-2;i++){
+			int sum=arr[i];
+			for(j=i+1;j<n;j++){
+				sum+=arr[j];
+				if(sum>=num)
+					break;
+			}
+			if(sum==num){
+				printf("%d %d\n",i+1,j+1);
+				return 0;
+			}	
+		}	    	
+		--t;
+	}
+	printf("%d",-1);
+}
